839ed803db14f2bb68b7106166defdc150394ac6
Shane <staggart@eastlink.ca>
2025-02-11 21:31:09 -0400

    Partial refactor of data combination.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 async function get_data() {
 
-    // Constants
-    const url = 'http://127.0.0.1:8082/src/data/all_time_sets.json';
+    // Constants (URLs)
+    const base_url = 'http://127.0.0.1:8082/src/data/';
+    const all_time_sets_url = base_url + 'all_time_sets.json';
+    const all_time_games_url = base_url + 'all_time_games.json';
+    const seasonal_sets_url = base_url + 'season_to_date_sets.json';
+    const seasonal_games_url = base_url + 'season_to_date_games.json';
+    const all_time_rankings_url = base_url + 'all_time_rankings.json';
+
+    // Constants (elements)
     const player_select_elements = document.querySelectorAll('select');
     const player_one_select = document.getElementById('player_one');
     const player_two_select = document.getElementById('player_two');
@@ -10,76 +17,113 @@ async function get_data() {
     // Attempt to pull data.
     try {
 
-        const response = await fetch(url);
+        const ats_response = await fetch(all_time_sets_url);
+        const atg_response = await fetch(all_time_games_url);
+        const stds_response = await fetch(seasonal_sets_url);
+        const stdg_response = await fetch(seasonal_games_url);
+        const atr_response = await fetch(all_time_rankings_url);
 
         // Failure to get a response.
-        if (!response.ok) {
-
-            throw new Error(`Response status: ${response.status}`);
-
-        }
+        if (!ats_response.ok) { throw new Error(`She's broke bahd! Response status: ${ats_response.status}`); }
+        if (!atg_response.ok) { throw new Error(`She's broke bahd! Response status: ${atg_response.status}`); }
+        if (!stds_response.ok) { throw new Error(`She's broke bahd! Response status: ${stds_response.status}`); }
+        if (!stdg_response.ok) { throw new Error(`She's broke bahd! Response status: ${stdg_response.status}`); }
+        if (!atr_response.ok) { throw new Error(`She's broke bahd! Response status: ${atr_response.status}`); }
 
         // Response is good, wait for data to pull.
-        const json = await response.json();
+        const ats_response_json = await ats_response.json();
+        const atg_response_json = await atg_response.json();
+        const stds_response_json = await stds_response.json();
+        const stdg_response_json = await stdg_response.json();
+        const atr_response_json = await atr_response.json();
 
-        // Store the full set of data.
-        const lifetime_set_data = json;
+        // Store the sets of data.
+        const lifetime_set_data = ats_response_json;
+        const lifetime_game_data = atg_response_json;
+        const seasonal_set_data = stds_response_json;
+        const seasonal_game_data = stdg_response_json;
+        const lifetime_rankings_data = atr_response_json;
 
-        // Build players array.
-        const players = json[0]
-        const players_array = [];
+        // Insert placeholder option elements.
+        player_one_select.innerHTML += `<option>Player One</option>`;
+        player_two_select.innerHTML += `<option>Player Two</option>`;
 
-        for (const key in players) {
-        
-            players_array.push([key, players[key]]);
-                
-        }
+        // Create option elements for each player from the fact sheet.
+        for (const key in atr_response_json) {
 
-        // Create option elements for each player.
-        players_array.forEach(player => {
+            let player = atr_response_json[key].Player;
+            let rank = atr_response_json[key].Rank
 
-            if(player[1] != ''){
+            if (atr_response_json[key].Player.includes(' | ')) {
 
-                player_one_select.innerHTML += `<option value="${player[0]}">${player[1]}</option>`;
-                player_two_select.innerHTML += `<option value="${player[0]}">${player[1]}</option>`;
+                player = atr_response_json[key].Player.split(' | ');
+                player = player[1];
 
             }
 
-        });
+            player_one_select.innerHTML += `<option value="${rank}">${player}</option>`;
+            player_two_select.innerHTML += `<option value="${rank}">${player}</option>`;
+                
+        }
+
+        // Debugging
+        console.group('All data sets');
+        console.log('--------------------------------');
+        console.log(`lifetime_set_data: `);
+        console.log(lifetime_set_data);
+        console.log(`lifetime_game_data: `);
+        console.log(lifetime_game_data);
+        console.log(`seasonal_set_data: `);
+        console.log(seasonal_set_data);
+        console.log(`seasonal_game_data: `);
+        console.log(seasonal_game_data);
+        console.log(`lifetime_rankings_data: `);
+        console.log(lifetime_rankings_data);
+        console.groupEnd('--------------------------------');
 
         // Watch select elements for changes.
         player_select_elements.forEach(select_element => {
 
             select_element.addEventListener("change", function() {
 
-                // player_one | player_two
-                let player_one_id = player_one_select.value;
-                let player_two_id = player_two_select.value;
+                // Store relevant player data.
                 let player_one_name = player_one_select.options[player_one_select.selectedIndex].innerText;
                 let player_two_name = player_two_select.options[player_two_select.selectedIndex].innerText;
-                let set_data = lifetime_set_data[player_one_id][player_two_id];
-                let set_result = set_data.replace(' -- ', '|').split('|');
-                let player_one_win_ratio = parseFloat(set_result[0]) / (parseFloat(set_result[0]) + parseFloat(set_result[1]));
-                let player_two_win_ratio = parseFloat(set_result[1]) / (parseFloat(set_result[0]) + parseFloat(set_result[1]));
-
+                let player_one_rank = player_one_select.options[player_one_select.selectedIndex].value;
+                let player_two_rank = player_two_select.options[player_two_select.selectedIndex].value;
+                
+                // Attempting to pull correct character data... need to establish fact sheet and combine data logically by player name otherwise the IDs don't match.
+                // let set_data = lifetime_set_data[player_one_id][player_two_id];
+                // let set_result = set_data.replace(' -- ', '|').split('|');
+                // let player_one_win_ratio = parseFloat(set_result[0]) / (parseFloat(set_result[0]) + parseFloat(set_result[1]));
+                // let player_two_win_ratio = parseFloat(set_result[1]) / (parseFloat(set_result[0]) + parseFloat(set_result[1]));
+                // let player_one_character = atr_response_json[0].Characters;
+                // let player_two_character = atr_response_json[1].Characters;
+                
                 console.group(`New player selected for ${select_element.name}!`);
-                console.log(`${player_one_name} (${player_one_id}) VS ${player_two_name} (${player_two_id})`);
-                console.log(`${set_result[0]} : ${set_result[1]}`);
+                console.log(`${player_one_name} (${player_one_rank}) VS ${player_two_name} (${player_two_rank})`);
+                // console.log(`${player_one_character} VS ${player_two_character}`);
+                // console.log(`${set_result[0]} : ${set_result[1]}`);
                 console.groupEnd();
 
                 data_output.innerHTML = 
                 `
                 <article class="player-stats">
+                    <p class="player-stats__player_character">${player_one_rank}</p>
+                    <p class="player-stats__player_character">${player_two_rank}</p>
+                </article>
+                <article class="player-stats">
                     <p class="player-stats__player_name">${player_one_name}</p>
                     <p class="player-stats__versus">VS</p>
                     <p class="player-stats__player_name">${player_two_name}</p>
                 </article>
-                <article class="player-stats">
-                    <p class="player-stats__sets">${(player_one_win_ratio * 100).toFixed(2)}%</p>    
-                    <p class="player-stats__sets">${set_result[0]} -- ${set_result[1]}</p>
-                    <p class="player-stats__sets">${(player_two_win_ratio * 100).toFixed(2)}%</p>
-                </article>
                 `;
+                // <article class="player-stats">
+                //     <p class="player-stats__sets">${(player_one_win_ratio * 100).toFixed(2)}%</p>    
+                //     <p class="player-stats__sets">${set_result[0]} -- ${set_result[1]}</p>
+                //     <p class="player-stats__sets">${(player_two_win_ratio * 100).toFixed(2)}%</p>
+                // </article>
+                // `;
 
             });
 
